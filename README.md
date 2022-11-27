05c5818741b0f5e5b5a93a6d83e1880922ba6ceb
Anton Medvedev <anton@medv.io>
2022-11-27 15:08:08 +0100

    Add -say flag

--- README.md
+++ README.md
@@ -2,26 +2,45 @@
 
 <p align="center"><img src="https://user-images.githubusercontent.com/141232/54696023-9ed03e00-4b5d-11e9-9c7b-d6f67691e70c.gif" width="450" alt="Screen shot"></p>
 
+## Install
+
+```bash
+go install github.com/antonmedv/countdown@latest
+```
+
+Or download prebuilt binary from [releases](https://github.com/antonmedv/countdown/releases).
+
 ## Usage
 
-Specify duration in go format `1h2m3s`.
+Specify duration in Go format `1h2m3s`.
 
 ```bash
 countdown 25s
 ```
 
-Or specify target time in [Kitchen time format](https://pkg.go.dev/time#pkg-constants). This starts a countdown to the matching time within the next 24 hours.
-For instance, if the current time would be 11:30AM, 
+Or specify target time: `02:15PM` or `14:15`. For instance, if the current time 
+would be 11:30, the next example would trigger a 2-minute countdown. 
+
 ```bash
-countdown 11:32AM
+countdown 11:32
 ```
 
-would trigger a 2 minute countdown.
-
-Add command with `&&` to run after countdown.
+Add a command with `&&` to run after the countdown.
 
 ```bash
 countdown 1m30s && say "Hello, world"
+```
+
+Count from up from the zero.
+
+```bash
+countdown -up 30s
+```
+
+Announce (via macOS `say` command) last 10 seconds
+
+```bash
+countdown -say 10s
 ```
 
 ## Key binding
@@ -30,12 +49,6 @@ countdown 1m30s && say "Hello, world"
 - `c` or `C`: To resume the countdown.
 - `Esc` or `Ctrl+C`: To stop the countdown without running next command.
 
-## Install
-
-```bash
-go install github.com/antonmedv/countdown
-```
-
 ## License
 
-MIT
+[MIT](LICENSE)
--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 	"time"
 
 	"github.com/nsf/termbox-go"
@@ -10,16 +13,14 @@ import (
 
 const (
 	usage = `
- countdown
+ countdown [-up] [-say] <duration>
 
- Usage:
-    countdown 25s
-    countdown 1m50s
-    countdown 3h40m50s
+ Usage
+  countdown 25s
+  countdown 14:15
+  countdown 02:15PM
 
- Options:
-    -up : count up from zero
-
+ Flags
 `
 	tick = time.Second
 )
@@ -31,6 +32,100 @@ var (
 	startDone      bool
 	startX, startY int
 )
+
+func main() {
+	countUp := flag.Bool("up", false, "count up from zero")
+	sayTheTime := flag.Bool("say", false, "announce the time left")
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) != 1 {
+		stderr(usage)
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
+	timeLeft, err := parseTime(args[0])
+
+	if err != nil {
+		timeLeft, err = time.ParseDuration(args[0])
+		if err != nil {
+			stderr("error: invalid duration or time: %v\n", args[0])
+			os.Exit(2)
+		}
+	}
+
+	err = termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+
+	queues = make(chan termbox.Event)
+	go func() {
+		for {
+			queues <- termbox.PollEvent()
+		}
+	}()
+	countdown(timeLeft, *countUp, *sayTheTime)
+}
+
+func start(d time.Duration) {
+	timer = time.NewTimer(d)
+	ticker = time.NewTicker(tick)
+}
+
+func stop() {
+	timer.Stop()
+	ticker.Stop()
+}
+
+func countdown(timeLeft time.Duration, countUp bool, sayTheTime bool) {
+	var exitCode int
+
+	start(timeLeft)
+
+	if countUp {
+		timeLeft = 0
+	}
+
+	draw(timeLeft)
+	if sayTheTime {
+		go say(timeLeft)
+	}
+
+loop:
+	for {
+		select {
+		case ev := <-queues:
+			if ev.Type == termbox.EventKey && (ev.Key == termbox.KeyEsc || ev.Key == termbox.KeyCtrlC) {
+				exitCode = 1
+				break loop
+			}
+			if ev.Ch == 'p' || ev.Ch == 'P' {
+				stop()
+			}
+			if ev.Ch == 'c' || ev.Ch == 'C' {
+				start(timeLeft)
+			}
+		case <-ticker.C:
+			if countUp {
+				timeLeft += tick
+			} else {
+				timeLeft -= tick
+			}
+			draw(timeLeft)
+			if sayTheTime {
+				go say(timeLeft)
+			}
+		case <-timer.C:
+			break loop
+		}
+	}
+
+	termbox.Close()
+	if exitCode != 0 {
+		os.Exit(exitCode)
+	}
+}
 
 func draw(d time.Duration) {
 	w, h := termbox.Size()
@@ -67,70 +162,26 @@ func format(d time.Duration) string {
 	return fmt.Sprintf("%02d:%02d:%02d", h, m, s)
 }
 
-func start(d time.Duration) {
-	timer = time.NewTimer(d)
-	ticker = time.NewTicker(tick)
-}
-
-func stop() {
-	timer.Stop()
-	ticker.Stop()
-}
-
-func countdown(timeLeft time.Duration, countUp bool) {
-	var exitCode int
-
-	start(timeLeft)
-
-	if countUp {
-		timeLeft = 0
-	}
-
-	draw(timeLeft)
-
-loop:
-	for {
-		select {
-		case ev := <-queues:
-			if ev.Type == termbox.EventKey && (ev.Key == termbox.KeyEsc || ev.Key == termbox.KeyCtrlC) {
-				exitCode = 1
-				break loop
-			}
-			if ev.Ch == 'p' || ev.Ch == 'P' {
-				stop()
-			}
-			if ev.Ch == 'c' || ev.Ch == 'C' {
-				start(timeLeft)
-			}
-		case <-ticker.C:
-			if countUp {
-				timeLeft += tick
-			} else {
-				timeLeft -= tick
-			}
-			draw(timeLeft)
-		case <-timer.C:
-			break loop
-		}
-	}
-
-	termbox.Close()
-	if exitCode != 0 {
-		os.Exit(exitCode)
+func say(d time.Duration) {
+	if d.Seconds() <= 10 {
+		cmd := exec.Command("say", fmt.Sprintf("%v", d.Seconds()))
+		_ = cmd.Run()
 	}
 }
 
-func getKitchenTimeDuration(date string) (time.Duration, error) {
-
-	targetTime, err := time.Parse(time.Kitchen, date)
+func parseTime(date string) (time.Duration, error) {
+	targetTime, err := time.Parse(time.Kitchen, strings.ToUpper(date))
 	if err != nil {
-		return time.Duration(0), err
+		targetTime, err = time.Parse("15:04", date)
+		if err != nil {
+			return time.Duration(0), err
+		}
 	}
 
 	now := time.Now()
 	originTime := time.Date(0, time.January, 1, now.Hour(), now.Minute(), now.Second(), 0, time.UTC)
 
-	// the time of day has already passed, so target tomorrow
+	// The time of day has already passed, so target tomorrow.
 	if targetTime.Before(originTime) {
 		targetTime = targetTime.AddDate(0, 0, 1)
 	}
@@ -138,36 +189,4 @@ func getKitchenTimeDuration(date string) (time.Duration, error) {
 	duration := targetTime.Sub(originTime)
 
 	return duration, err
-}
-
-func main() {
-	if len(os.Args) < 2 || len(os.Args) > 3 {
-		stderr(usage)
-		os.Exit(2)
-	}
-
-	timeLeft, err := getKitchenTimeDuration(os.Args[1])
-
-	if err != nil {
-
-		timeLeft, err = time.ParseDuration(os.Args[1])
-		if err != nil {
-			stderr("error: invalid duration or kitchen time: %v\n", os.Args[1])
-			os.Exit(2)
-		}
-	}
-
-	err = termbox.Init()
-	if err != nil {
-		panic(err)
-	}
-
-	queues = make(chan termbox.Event)
-	go func() {
-		for {
-			queues <- termbox.PollEvent()
-		}
-	}()
-	countUp := len(os.Args) == 3 && os.Args[2] == "-up"
-	countdown(timeLeft, countUp)
 }
